cc3fcc8785c29b03667a886158423db59ecfe567
Mctom_Spdo <mctom.spdo@gmail.com>
2021-03-25 12:57:26 +0100

    NodeJS Alpha 1.0

---
+++ code/NodeJS/server.js
@@ -0,0 +1,30 @@
+console.log('[INFO]: Starting Webserver!');
+
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const port = 80; //default WebPort
+
+app.use(express.static('public-root/webserver-root'), express.urlencoded({extended: true}));
+
+app.listen(port, () => {
+    console.log("[INFO]: Done!");
+    console.log(`[INFO]: Server is running on port ${port} (http://localhost:${port}/ )`);
+})
+
+/********************************* GETS ****************************************/
+
+app.get('/API/ping', (req, res) => {
+    res.send('pong');
+});
+
+app.get('/API/version', (req, res) => {
+    res.send('Version: Alpha 1.0');
+});
+
+/******************************** 404 *****************************************/
+
+app.use((req, res, next) => {//404 page Error (https://expressjs.com/en/starter/faq.html)
+    res.status(404).send(fs.readFileSync('./public-root/webserver-root/404.html', 'utf8'));
+});
