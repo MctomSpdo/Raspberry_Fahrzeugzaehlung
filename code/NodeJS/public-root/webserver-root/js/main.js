7ffe7da2a40d520345102edb1d854641d9a2c499
Toldo Sebastian <it190173@EDU.HTL-LEONDING.AC.AT>
2021-06-17 13:29:30 +0200

    Jo

--- code/NodeJS/public-root/webserver-root/js/main.js
+++ code/NodeJS/public-root/webserver-root/js/main.js
@@ -1,3 +1,25 @@
+//Fetch zum Empfangen der gezählten Daten
+
+fetch('http://localhost:3000/')
+.then ((response) =>{
+  return response.json();
+})
+.then((data)=>{
+  console.log(data);
+})
+.catch((error) =>{
+  console.log('Error:', error);
+})
+
+ //Versuch einer Datumsanzeige
+ /*let date = new Date();
+ let day = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+*/
+
+//Zeitspanne
+
 let i = 0;
 
 let labels = [
@@ -28,6 +50,7 @@ let labels = [
   '24:00',
 ];
 
+//Variablen für die Charts
 let data1 = {
   labels: labels,
   datasets: [{
@@ -54,16 +77,101 @@ let data2 = {
     label: 'LKW ',
     backgroundColor: 'rgb(30,30,30)',
     borderColor: 'rgb(30, 30, 30)',
-    data: [15, 9, 4, 3, 19, 27, 33],
+    data: [15, 9, 45, 3, 19, 27, 33],
+  }]
+};
+let data3 = {
+  labels: labels,
+  datasets: [{
+    label: 'Auto',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 10, 5, 2, 20, 30, 45],
+  }, {
+    label: 'LKW ',
+    backgroundColor: 'rgb(30,30,30)',
+    borderColor: 'rgb(30, 30, 30)',
+    data: [15, 9, 40, 30, 19, 27, 33],
+  }]
+};
+let data4 = {
+  labels: labels,
+  datasets: [{
+    label: 'Auto',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 10, 5, 2, 20, 30, 45],
+  }, {
+    label: 'LKW ',
+    backgroundColor: 'rgb(30,30,30)',
+    borderColor: 'rgb(30, 30, 30)',
+    data: [15, 90, 40, 3, 19, 27, 33],
+  }]
+};
+let data5 = {
+  labels: labels,
+  datasets: [{
+    label: 'Auto',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 10, 5, 2, 20, 30, 45],
+  }, {
+    label: 'LKW ',
+    backgroundColor: 'rgb(30,30,30)',
+    borderColor: 'rgb(30, 30, 30)',
+    data: [15, 9, 4, 30, 19, 27, 33],
+  }]
+};
+let data6 = {
+  labels: labels,
+  datasets: [{
+    label: 'Auto',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 10, 5, 2, 20, 30, 45],
+  }, {
+    label: 'LKW ',
+    backgroundColor: 'rgb(30,30,30)',
+    borderColor: 'rgb(30, 30, 30)',
+    data: [15, 9, 40, 3, 19, 27, 33],
+  }]
+};
+let data7 = {
+  labels: labels,
+  datasets: [{
+    label: 'Auto',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 10, 5, 2, 20, 30, 45],
+  }, {
+    label: 'LKW ',
+    backgroundColor: 'rgb(30,30,30)',
+    borderColor: 'rgb(30, 30, 30)',
+    data: [15, 90, 4, 3, 19, 27, 33],
   }]
 };
 
-
+//Verzögertes Laden der Charts
 chart(data1);
 
 setTimeout( ()=> {
   chart(data2);
+ }, 1000)
+ setTimeout( ()=> {
+  chart(data3);
  }, 2000)
+ setTimeout( ()=> {
+  chart(data4);
+ }, 3000)
+ setTimeout( ()=> {
+  chart(data5);
+ }, 4000)
+ setTimeout( ()=> {
+  chart(data6);
+ }, 5000)
+ setTimeout( ()=> {
+  chart(data7);
+ }, 6000)
 
 
 
@@ -79,11 +187,19 @@ function chart(data) {
   let config = {
     type: 'line',
     data,
-    options: {}
-  };
+    
+      };
+      
   
+
+ //Verusch einer Datumsanzeige
+
+  //document.getElementsByClassName(`date${i}`).innerHTML += `${day} ${month} ${year}`;
+  //document.getElementsByClassName(`date${i}`).style.color = "#ff0000";
+
    document.getElementById(i).innerHTML += `<canvas id="myChart${i}"></canvas>`;
    
+
    console.log(i);
    console.log(document.getElementById(`myChart${i}`))
   
